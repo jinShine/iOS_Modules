beba8fd5eecf8f80bcdcb3b7c735c12316b2bfec
jinnify <seungjin429@gmail.com>
2020-02-08 19:48:43 +0900

    Size Class Module 예제

---
+++ SizeClassModule/SizeClassModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClassModule
+//
+//  Created by seungjin on 2020/02/08.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
