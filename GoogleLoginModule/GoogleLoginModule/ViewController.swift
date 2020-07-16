b6c02c5f2f6dc3d03ec90ee9ad6c70652e6c5d1f
jinnify <seungjin429@gmail.com>
2020-07-16 20:54:33 +0900

    create google login module

---
+++ GoogleLoginModule/GoogleLoginModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleLoginModule
+//
+//  Created by Jinnify on 2020/07/16.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
