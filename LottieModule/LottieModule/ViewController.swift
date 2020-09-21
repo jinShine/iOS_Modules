e6230eac882378674619358201a263d166892b39
jinnify <seungjin429@gmail.com>
2020-09-21 23:53:51 +0900

    create Lottie Module

---
+++ LottieModule/LottieModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieModule
+//
+//  Created by Jinnify on 2020/09/21.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
