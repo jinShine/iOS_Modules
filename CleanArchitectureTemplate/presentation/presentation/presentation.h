8c71699d414c5b49ed4be6f10a9a9153995a50f8
jinShine <seungjin429@gmail.com>
2020-10-11 00:21:45 +0900

    각 Layer들 추가

---
+++ CleanArchitectureTemplate/presentation/presentation/presentation.h
@@ -0,0 +1,19 @@
+//
+//  presentation.h
+//  presentation
+//
+//  Created by Buzz.Kim on 2020/10/11.
+//  Copyright © 2020 jinnify. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for presentation.
+FOUNDATION_EXPORT double presentationVersionNumber;
+
+//! Project version string for presentation.
+FOUNDATION_EXPORT const unsigned char presentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <presentation/PublicHeader.h>
+
+
