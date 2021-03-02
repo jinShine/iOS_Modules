c850d3f133d0812a673a3132901f8ee1937a675c
jinShine <seungjin429@naver.com>
2021-03-02 22:16:14 +0900

    - sub pagertabviewcontroller 추가

--- UIComponent/UIComponent/Component/PagerTabView/SubPagerTabViewController.swift
+++ UIComponent/UIComponent/Component/PagerTabView/SubPagerTabViewController.swift
@@ -8,4 +8,32 @@
 import UIKit
 import XLPagerTabStrip
 
+// TODO: BaseViewController로 변경해야됨
+open class SubPagerTabViewController: UIViewController {
 
+  var item: Any?
+  
+  open override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
+
+// MARK: - Helper methods
+
+extension SubPagerTabViewController: Resetable {
+  
+  public func reset() {
+    
+  }
+}
+
+// MARK: - Required
+
+extension SubPagerTabViewController: IndicatorInfoProvider {
+  
+  public func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+    return IndicatorInfo(title: "")
+  }
+}
