b18538c113b50b135c4eb43d72a88414ba65c5c9
jinShine <seungjin429@naver.com>
2021-03-07 11:32:25 +0900

    - create code snippets(ViewController, ViewModel)

---
+++ CodeSnippets/CodeSnippets/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  CodeSnippets
+//
+//  Created by buzz on 2021/03/07.
+//
+
+import RxCocoa
+import RxSwift
+import UIKit
+import Common
+
+final class <#ViewController#>: ViewController {
+
+  // MARK: - Constant
+
+  typealias ViewModelType = <#ViewModel#>Bindable
+
+  // MARK: - UI Properties
+
+  // MARK: - Properties
+
+  var viewModel: ViewModelType
+  var navigator: Navigator
+
+  // MARK: - Initialize
+
+  init(viewModel: ViewModelType, navigator: Navigator) {
+    self.viewModel = viewModel
+    self.navigator = navigator
+    super.init()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Setup
+
+  override func setupUI() {
+    super.setupUI()
+  }
+
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+
+  // MARK: - Binding methods
+
+  override func inputBinding() {
+    super.inputBinding()
+  }
+
+  override func outputBinding() {
+    super.outputBinding()
+  }
+}
+
+// MARK: - Helper methods
+
+extension <#ViewController#> { }
---
+++ CodeSnippets/CodeSnippets/ViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ViewModel.swift
+//  CodeSnippets
+//
+//  Created by buzz on 2021/03/07.
+//
+
+import RxCocoa
+import RxSwift
+import UIKit
+import Common
+
+protocol <#ViewModel#>Bindable {
+  // Input
+
+  // Output
+}
+
+final class <#ViewModel#>: ViewModel, <#ViewModel#>Bindable {
+
+  // MARK: Input
+
+  // MARK: Output
+
+  // MARK: - Properties
+
+  // MARK: - Initialize
+
+  override init(
+  ) {
+
+  }
+}
+
+// MARK: - Helper methods
+
+extension <#ViewModel#> { }
