b7a6b01dcd52998ea87eda233a4948634b417aba
jinShine <seungjin429@naver.com>
2021-04-17 23:58:40 +0900

    - Delegate Proxy 아닌 버전 추가

--- NaverModule/NaverModule/NaverManasger.swift
+++ NaverModule/NaverModule/NaverManasger.swift
@@ -209,3 +209,86 @@ extension Reactive where Base: NaverAppService {
       .asObserver()
   }
 }
+
+
+// Delegate 프록시 아닌 버전
+
+
+extension NaverAppService: NaverThirdPartyLoginConnectionDelegate {
+
+  /// 네이버으로부터 리다이렉트 된 URL 인지 체크 후 요청 결과를 처리
+  func naverLoginOpenURL(openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    if let url = URLContexts.first?.url {
+      NaverThirdPartyLoginConnection.getSharedInstance()?.receiveAccessToken(url)
+    }
+  }
+
+  /// 로그인
+  func login() -> Observable<Void> {
+    return Observable.create { [weak self] observer -> Disposable in
+      self?.naver.requestThirdPartyLogin()
+      observer.onCompleted()
+      return Disposables.create()
+    }
+  }
+
+  /// 로그아웃
+  func logout() -> Observable<Void> {
+    return Observable.create { [weak self] observer -> Disposable in
+      self?.naver.requestDeleteToken()
+      observer.onCompleted()
+      return Disposables.create()
+    }
+  }
+
+  func requestUserEmail() -> Observable<(token: LificToken, email: String)> {
+    return Observable<(token: LificToken, email: String)>.create { observer -> Disposable in
+      guard self.naver.isValidAccessTokenExpireTimeNow() else {
+        observer.onCompleted()
+        return Disposables.create()
+      }
+
+      let tokenType = self.naver.tokenType ?? "Bearer"
+      let accessToken = self.naver.accessToken ?? ""
+      let authorization = "\(tokenType) \(accessToken)"
+      let url = URL(string: "https://openapi.naver.com/v1/nid/me")!
+
+      AF.request(url, method: .get, encoding: JSONEncoding.default, headers: ["Authorization": authorization])
+        .responseJSON { response in
+          log.debug(response)
+          if let result = response.value as? [String: Any] {
+            if let responseDict = result["response"] as? [String: Any] {
+              let email = responseDict["email"] as? String ?? ""
+              let token = LificToken(accessToken: accessToken)
+              observer.onNext((token, email))
+              observer.onCompleted()
+            }
+          }
+        }
+
+      return Disposables.create()
+    }
+  }
+
+  // MARK: - Naver Login Delegate
+
+  func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+    requestUserEmail()
+      .bind(to: userInfo)
+      .disposed(by: rx.disposeBag)
+  }
+
+  func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+    requestUserEmail()
+      .bind(to: userInfo)
+      .disposed(by: rx.disposeBag)
+  }
+
+  func oauth20ConnectionDidFinishDeleteToken() {
+    naver.requestDeleteToken()
+  }
+
+  func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection, didFailWithError error: Error) {
+    errorMessage.onNext(error)
+  }
+}
