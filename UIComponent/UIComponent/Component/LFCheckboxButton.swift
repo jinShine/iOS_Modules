fd57203e31f3cee7abe2c70b15c021041db2564f
jinShine <seungjin429@naver.com>
2021-09-08 23:37:32 +0900

    - Checkbox 로직 수정

--- UIComponent/UIComponent/Component/LFCheckboxButton.swift
+++ UIComponent/UIComponent/Component/LFCheckboxButton.swift
@@ -5,89 +5,128 @@
 //  Created by buzz on 2021/02/05.
 //
 
+import NSObject_Rx
 import RxCocoa
 import RxSwift
 import UIKit
-import NSObject_Rx
 
 /**
  [UI 제플린에서 보기](https://zpl.io/2jglkgA)
  */
 
 public class LFCheckboxButton: Button {
-  
-  // MARK: - Properties
-  
-  public override var isSelected: Bool {
-    didSet {
-      changeCheckboxState(by: isSelected)
+
+  // MARK: - Style
+
+  public enum LFCheckboxType {
+    case rectangle
+    case circle
+
+    var size: CGFloat {
+      switch self {
+      case .rectangle:
+        return 18
+      case .circle:
+        return 24
+      }
     }
   }
-  
+
+  // MARK: - Properties
+
+  public lazy var type: LFCheckboxType = .rectangle {
+    didSet {
+      changeImage(for: type)
+      updateSize(type.size)
+    }
+  }
+
   // MARK: - Initialize
-  
-  public override init(frame: CGRect) {
-    super.init(frame: frame)
+
+  public init(type checkboxType: LFCheckboxType) {
+    super.init(frame: .zero)
+    type = checkboxType
     setupUI()
+    setupConstraints()
     bind()
   }
-  
+
+  override public init(frame: CGRect) {
+    super.init(frame: frame)
+    setupUI()
+    setupConstraints()
+    bind()
+  }
+
   public required init?(coder: NSCoder) {
     super.init(coder: coder)
     setupUI()
+    setupConstraints()
     bind()
   }
-  
+
   // MARK: - Setup
-  
+
   private func setupUI() {
-    setDisabledImage()
-    updateUI()
+    changeImage(for: type)
   }
-  
-  private func updateUI() {
-    setNeedsDisplay()
+
+  private func setupConstraints() {
+    self.snp.makeConstraints {
+      $0.size.equalTo(type.size)
+    }
   }
-  
 }
 
 // MARK: - Binding methods
 
 extension LFCheckboxButton {
-  
+
   private func bind() {
     rx.tap
-      .bind { [weak self] in self?.isSelectedToggle() }
-      .disposed(by: rx.disposeBag)
+      .bind { [weak self] in
+        self?.isSelectedToggle {
+          self?.toggleAnimation()
+        }
+      }.disposed(by: rx.disposeBag)
   }
-  
 }
 
 // MARK: - Helper methods
 
 extension LFCheckboxButton {
-  
-  private func isSelectedToggle() {
+
+  public func updateSize(_ size: CGFloat) {
+    self.snp.updateConstraints {
+      $0.size.equalTo(size)
+    }
+  }
+
+  private func changeImage(for type: LFCheckboxType) {
+    var selectedImage: UIImage?
+    var unselectedImage: UIImage?
+
+    switch type {
+    case .rectangle:
+      selectedImage = Theme.image.checkboxSelected.withRenderingMode(.alwaysOriginal)
+      unselectedImage = Theme.image.checkboxDisabled.withRenderingMode(.alwaysOriginal)
+
+    case .circle:
+      selectedImage = Theme.image.checkboxCircleSelected.withRenderingMode(.alwaysOriginal)
+      unselectedImage = Theme.image.checkboxCircleDisabled.withRenderingMode(.alwaysOriginal)
+    }
+
+    setImage(unselectedImage, for: .normal)
+    setImage(selectedImage, for: .selected)
+  }
+
+  private func isSelectedToggle(completion: @escaping () -> Void) {
     isSelected = !isSelected
-    toggleAnimation()
+    completion()
   }
-  
-  private func changeCheckboxState(by isSelected: Bool) {
-    isSelected ? setSelectedImage() : setDisabledImage()
-  }
-  
-  private func setSelectedImage() {
-    let image = Theme.image.checkboxSelected.withRenderingMode(.alwaysOriginal)
-    setImage(image, for: .normal)
-  }
-  
-  private func setDisabledImage() {
-    let image = Theme.image.checkboxDisabled.withRenderingMode(.alwaysOriginal)
-    setImage(image, for: .normal)
-  }
-  
+
   private func toggleAnimation() {
-    UIView.animate(withDuration: 0.1) {
+    UIView.animate(withDuration: Constants.Animation.duration) {
       self.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
     } completion: { _ in
       self.transform = .identity
